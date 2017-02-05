e0d646208e920fa67caf5ac381ee620fdba91ca4
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-05 18:30:06 +0000

    Create main menu

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : Singleton<LevelManager> {
+
+	public void LoadNextScene ()
+    {
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit ();
+    }
+
+}
