0dd0dabdae3fee5082bc1e64c53a578ed91437e3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-05 21:02:39 +0000

    Ball bounce audio now functional

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private GvrAudioSource source;
+    private Rigidbody rb;
+
+    private void Awake ()
+    {
+        source = GetComponent<GvrAudioSource> ();
+        rb = GetComponent<Rigidbody> ();
+    }
+
+	private void OnCollisionEnter (Collision other)
+    {
+        float volume = Mathf.Min (rb.velocity.magnitude / 10, 1);
+        source.PlayOneShot(source.clip, volume);
+    }
+
+}
