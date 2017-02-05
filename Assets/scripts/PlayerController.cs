5a8fae87511d5d27686eec5c398ea077adc7eace
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-05 09:40:56 +0000

    Add ability to launch ball

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+    private GameObject ballPrefab;
+
+    private Camera cam;
+    private Vector3 ballOffset = new Vector3 (0, -1, 2);
+    private float ballLaunchAngleOffsetDeg = 30f;
+    private float ballSpeed = 20;
+
+    private void Awake ()
+    {
+        cam = GetComponentInChildren<Camera> ();
+    }
+
+    private void Update ()
+    {
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            LaunchBall ();
+        }
+    }
+
+    private void LaunchBall ()
+    {
+        GameObject ball = Instantiate (ballPrefab);
+        Rigidbody ballRb = ball.GetComponent<Rigidbody> ();
+        ball.transform.position = transform.position + ballOffset;
+        float launchAngleRad = ballLaunchAngleOffsetDeg * Mathf.Deg2Rad;
+        Vector3 ballLaunchAngle = Vector3.RotateTowards (cam.transform.forward, Vector3.up, launchAngleRad, 0);
+        ballLaunchAngle *= ballSpeed;
+        ballRb.velocity = ballLaunchAngle;
+    }
+
+}
